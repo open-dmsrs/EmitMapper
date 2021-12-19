fdf3177ef3c029a5125c70b672bd2136a1145b35
68681395 <happycoder@foxmail.com>
2021-12-20 01:07:43 +0800

    add a test for MapEnum

--- EmitMapper.xUnit/MappingList.cs
+++ EmitMapper.xUnit/MappingList.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoFixture;
 using AutoFixture.Xunit2;
 using EmitMapper.MappingConfiguration;
 using EmitMapper.MappingConfiguration.MappingOperations;
 using EmitMapper.MappingConfiguration.MappingOperations.Interfaces;
+using EmitMapper.Utils;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -75,7 +79,7 @@ public class MappingList
 
     [Theory]
     [AutoData]
-    public void TestCopyArrayList(List<FromClass> list)
+    public void TestCopyList1(List<FromClass> list)
     {
         ArrayList listFrom = new(list.ToArray());
 
@@ -132,7 +136,7 @@ public class MappingList
     [AutoData]
     public void TestCopyEnum(List<FromClass> list)
     {
-        
+
 
         _testOutputHelper.WriteLine(list.Count.ToString());
 
@@ -181,6 +185,84 @@ public class MappingList
         }
     }
 
+    [Fact] 
+    public void TestDefaultConfigWithEnum()
+    {
+        Fixture fixture = new Fixture();
+        List<B2Source> list = fixture.CreateMany<B2Source>(50).ToList();
+
+        _testOutputHelper.WriteLine(list.Count.ToString());
+        
+        var mapper = ObjectMapperManager.DefaultInstance.GetMapper<B2Source, A2Destination>();
+
+        var tolist = mapper.MapEnum(list);
+        var f = list.GetEnumerator();
+        var t = tolist.GetEnumerator();
+        while (f.MoveNext() && t.MoveNext())
+        {
+            _testOutputHelper.WriteLine(f.Current.N5.ToString());
+            var fv = ReflectionUtils.GetPublicFieldsAndProperties(typeof(B2Source))
+                   .Select(m => new
+                   {
+                       m.Name,
+                       FValue = (m as FieldInfo)?.GetValue(f.Current)?.ToString()
+                   });
+
+            var tv = ReflectionUtils.GetPublicFieldsAndProperties(typeof(A2Destination)).Select(m => new
+            {
+                m.Name,
+                TValue = (m as FieldInfo)?.GetValue(t.Current)?.ToString()
+            });
+
+            var result = fv
+                .Join(tv, a => a.Name, b => b.Name, (a, b) => new { a.Name,  a.FValue,  b.TValue });
+
+            foreach (var temp in result)
+            {
+                Assert.Equal(temp.FValue, temp.TValue);
+            }
+        }
+    }
+
+    public class B2Source
+    {
+        public byte N4;
+        public decimal? N5;
+        public float N6;
+
+        public int N1;
+        public int N7;
+        public long N2;
+        public short N3;
+        public string Str1;
+        public string Str2;
+        public string Str3;
+        public string Str4;
+        public string Str5;
+        public string Str6;
+        public string Str7;
+        public string Str8;
+        public string Str9;
+    }
+    public class A2Destination
+    {
+        public int N1;
+        public int N2;
+        public int N3;
+        public int N4;
+        public int N5;
+        public int N6;
+        public int N7;
+        public string Str1;
+        public string Str2;
+        public string Str3;
+        public string Str4;
+        public string Str5;
+        public string Str6;
+        public string Str7;
+        public string Str8;
+        public string Str9;
+    }
     public class ToClass
     {
         public string Message;
