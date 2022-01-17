bf25081a1cdc257a84a80f4016e9cc587b54cd9b
Jimbo Tang <happycoder@foxmail.com>
2022-01-17 18:07:52 +0800

    -

---
+++ EmitMapper/Utils/VersatileLambda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EmitMapper.Utils;
+
+public class VersatileLambda<T> where T : class
+{
+    private readonly Expression<T> _expression;
+    private readonly Lazy<T> _funcLazy;
+
+    public VersatileLambda(Expression<T> expression)
+    {
+        if (expression == null) throw new ArgumentNullException(nameof(expression));
+        _expression = expression;
+        _funcLazy = new Lazy<T>(expression.Compile);
+    }
+
+    public static implicit operator Expression<T>(VersatileLambda<T> lambda)
+    {
+        return lambda?._expression;
+    }
+
+    public static implicit operator T(VersatileLambda<T> lambda)
+    {
+        return lambda?._funcLazy.Value;
+    }
+
+    public Expression<T> AsExpression()
+    {
+        return this;
+    }
+
+    public T AsLambda()
+    {
+        return this;
+    }
+}
---
+++ EmitMapper/Utils/WhereConstraint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EmitMapper.Utils;
+
+public class WhereConstraint<T> : VersatileLambda<Func<T, bool>>
+{
+    public WhereConstraint(Expression<Func<T, bool>> lambda)
+        : base(lambda)
+    {
+    }
+}
