d50aa170864ac6845c391ecf2f56142bb9ffbd2b
Jimbo Tang <happycoder@foxmail.com>
2022-03-03 12:19:41 +0800

    -

--- EmitMapper/TryPrintConstant.cs
+++
@@ -1,6 +0,0 @@
-using System.Linq.Expressions;
-
-namespace EmitMapper;
-
-/// <summary>Output the constant to C# string or should return `null`</summary>
-public delegate string TryPrintConstant(ConstantExpression e);
