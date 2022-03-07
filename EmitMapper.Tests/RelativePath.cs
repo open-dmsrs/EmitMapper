60d77f80f27626614ce70c46594024b7042cb068
Jimbo Tang <happycoder@foxmail.com>
2022-03-07 18:21:42 +0800

    test readme.md file in package

---
+++ EmitMapper.Tests/RelativePath.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using System.IO;
+using Xunit;
+
+namespace EmitMapper.Tests;
+
+public class RelativePath
+{
+  [Fact]
+  public void GetRelativePath()
+  {
+    var parent = @"c:\";
+    var sub = @"C:\Users\Default";
+    var result = Path.GetRelativePath(parent, sub);
+    result.ShouldBe(@"Users\Default");
+
+    result = Path.GetRelativePath(sub, parent);
+    result.ShouldBe(@"..\..");
+  }
+
+}
